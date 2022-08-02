94091c7b3973de73183f10b9340ea41becd2a3ee
Glen <glen84_dr@hotmail.com>
2022-08-03 01:02:20 +0400

    lllllllllllll

--- script/main.py
+++ script/main.py
@@ -3,8 +3,7 @@ from read import data_final
 import numpy as np
 from choos_columns_rows import CreateColumsRows
 
-output = OutputBlocs(1,2,14,data_final).adding_col_and_row()
+output = OutputBlocs(1,2,0,14,data_final).adding_col_and_row()
 print(output[0:21])
 
 column_selected = CreateColumsRows().select_periodes(0,np.array(data_final))
-print(column_selected)
--- script/output.py
+++ script/output.py
@@ -1,24 +1,33 @@
 import numpy as np
 from choos_columns_rows import CreateColumsRows
 from chaines import TransposingBlocs
+from itertools import repeat
 
 class OutputBlocs:
-    def __init__(self, col_nb, first_col_of_row, step, dt):
+    def __init__(self, col_nb, first_col_of_row, col_periode, step, dt):
         self.col_nb = col_nb
         self.first_col_of_row = first_col_of_row
+        self.col_periode = col_periode
         self.step = step
         self.dt = dt
 
     last = []
+    new_list=[]
     def adding_titles(self):
         last_row_of_col = self.step+1
-        return np.append("Chaines",CreateColumsRows().select_row_from_col(self.col_nb,last_row_of_col,np.array(self.dt)))
+        return np.append( ["Mois","Chaines"],CreateColumsRows().select_row_from_col(self.col_nb,last_row_of_col,np.array(self.dt)))
 
     def adding_col_and_row(self):
         x = TransposingBlocs(self.step,np.array(self.dt)).transpos_fc()
         y = CreateColumsRows().select_column_from_row(self.first_col_of_row,np.array(self.dt))
+        z = CreateColumsRows().select_periodes(self.col_periode,np.array(self.dt))[1:]
+
+        for k in range(len(z)):
+            self.new_list.append([z[k] for l in range(len(y))])
+        print(self.new_list)
 
         for i in range(len(x)):
             for j in range(len(y)):
-                self.last.append(np.append(y[j],x[i][j]))
+                self.last.append(np.append(x[j],x[i][j]))
+
         return np.array(np.vstack([self.adding_titles(), self.last]))
