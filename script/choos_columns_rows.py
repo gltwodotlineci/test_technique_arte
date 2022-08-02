f41e78586a4d7421bd7a8a94840124b8b5391001
Glen <glen84_dr@hotmail.com>
2022-08-02 23:14:20 +0400

    selecting columns for the periodes

--- script/choos_columns_rows.py
+++ script/choos_columns_rows.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class CreateColumsRows:
     def select_column_from_row(self,first_col_of_row, dt):
@@ -5,3 +7,7 @@ class CreateColumsRows:
 
     def select_row_from_col(self,col_nb,last_row_of_cl,dt):
         return (dt[1:last_row_of_cl,col_nb])
+
+    def select_periodes(self, col_nb, dt):
+        x = (dt[:,col_nb])
+        return list(dict.fromkeys(x))
--- script/main.py
+++ script/main.py
@@ -1,5 +1,10 @@
 from output import OutputBlocs
 from read import data_final
+import numpy as np
+from choos_columns_rows import CreateColumsRows
 
 output = OutputBlocs(1,2,14,data_final).adding_col_and_row()
 print(output[0:21])
+
+column_selected = CreateColumsRows().select_periodes(0,np.array(data_final))
+print(column_selected)
