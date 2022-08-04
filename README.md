926f261ebbd99d968a40d8fe536a20c674171b71
Glen <glen84_dr@hotmail.com>
2022-08-04 14:54:08 +0400

    adding periode colon to data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-##ARTE channel needs help. 
+## ARTE channel needs help.
 1. They have a large **data table in csv format** and they need to make a study with these data. But there is a problem the data needs some transformation before the study. Can you help them by building some methods with python to bring the data in the right standard?
 
     * Your work will consist in transposing thee data for each month. *Be careful* they need a transpose mounth by mounth and not all the data at once
--- script/output.py
+++ script/output.py
@@ -1,7 +1,6 @@
 import numpy as np
 from choos_columns_rows import CreateColumsRows
 from chaines import TransposingBlocs
-from itertools import repeat
 
 class OutputBlocs:
     def __init__(self, col_nb, first_col_of_row, col_periode, step, dt):
@@ -15,19 +14,20 @@ class OutputBlocs:
     new_list=[]
     def adding_titles(self):
         last_row_of_col = self.step+1
-        return np.append( ["Mois","Chaines"],CreateColumsRows().select_row_from_col(self.col_nb,last_row_of_col,np.array(self.dt)))
+        return np.append(["Chaines"], CreateColumsRows().select_row_from_col(self.col_nb,last_row_of_col,np.array(self.dt)))
 
     def adding_col_and_row(self):
         x = TransposingBlocs(self.step,np.array(self.dt)).transpos_fc()
         y = CreateColumsRows().select_column_from_row(self.first_col_of_row,np.array(self.dt))
         z = CreateColumsRows().select_periodes(self.col_periode,np.array(self.dt))[1:]
-
-        for k in range(len(z)):
-            self.new_list.append([z[k] for l in range(len(y))])
-        print(self.new_list)
+        z = np.concatenate([([x]*len(y)) for x in z], axis=0)
+        z = np.append("Mois",z)
 
         for i in range(len(x)):
             for j in range(len(y)):
-                self.last.append(np.append(x[j],x[i][j]))
+                self.last.append(np.append(y[j],x[i][j]))
 
-        return np.array(np.vstack([self.adding_titles(), self.last]))
+        final = np.array(np.vstack([self.adding_titles(),self.last]))
+        for k in range(len(final)):
+            self.new_list.append(np.append(z[k], final[k]))
+        return np.array(self.new_list)
